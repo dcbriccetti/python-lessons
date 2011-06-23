ef0b914b055d5ba878d5e3459d73768c12bffeff
Warren Sande <cp4khelp@yahoo.com>
2011-06-23 15:03:13 -0700

    New example:  loop-through-string

---
+++ simple/src/loop-through-string.py
@@ -0,0 +1,9 @@
+print('Looping through a list')
+fruits = ['apple','banana','orange','pineapple','mango']
+for i in fruits:
+    print("Here's an item:",i)
+print()
+print('Looping through a string')
+name='Billy Jones'
+for i in name:
+    print("Here's an item:",i)
