0e9b446a14efc5c340a61c7fead791c89ded839d
daveb <daveb@davebsoft.com>
2011-07-21 10:49:48 -0700

    Allow exiting gracefully. Use format instead of % to format string.

--- simple/src/mathquiz.py
+++ simple/src/mathquiz.py
@@ -1,8 +1,12 @@
 from random import randint
 
+print('Enjoy a math quiz, and respond 0 to stop.')
+
 while True:
     m1 = randint(2, 11)
     m2 = randint(2, 11)
     prod = m1 * m2
-    ans = int(input('What is %d * %d? ' % (m1, m2)))
+    ans = int(input('What is {0} * {1} '.format(m1, m2)))
+    if ans == 0:
+        break
     print('Right' if ans == prod else 'Wrong, the answer was %d' % prod)
