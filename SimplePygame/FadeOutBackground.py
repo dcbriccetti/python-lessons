62f22584c15da934744dbc92c711407b08dc638d
daveb <daveb@davebsoft.com>
2012-01-14 09:13:23 -0800

    Remove main methods. Add pygame.quit() call.

--- SimplePygame/FadeOutBackground.py
+++ SimplePygame/FadeOutBackground.py
@@ -1,38 +1,35 @@
 import pygame
 from pygame.locals import *
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 400), 0)
-    pygame.display.set_caption('Move Bee Right and Notice the Fadeout')
-    bee = pygame.image.load('bee1.png').convert_alpha()
-    beeX = 0
-    beeY = 0
-    bgsound = pygame.mixer.Sound('groovy.wav')
-    bgsound.play(loops=10)
+pygame.init()
+screen = pygame.display.set_mode((600, 400), 0)
+pygame.display.set_caption('Move Bee Right and Notice the Fadeout')
+bee = pygame.image.load('bee1.png').convert_alpha()
+beeX = 0
+beeY = 0
+bgsound = pygame.mixer.Sound('groovy.wav')
+bgsound.play(loops=10)
 
-    loop = True
-    while loop:
-        for event in pygame.event.get():
-            if event.type == QUIT \
-                or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                loop = False
+loop = True
+while loop:
+    for event in pygame.event.get():
+        if event.type == QUIT \
+            or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            loop = False
 
-        keystate = pygame.key.get_pressed()
-        if keystate[K_RIGHT]:
-            bgsound.fadeout(5000)
-            beeX += 5
-        if keystate[K_LEFT]:
-            beeX -= 5
-        if keystate[K_UP]:
-            beeY -= 5
-        if keystate[K_DOWN]:
-            beeY += 5
+    keystate = pygame.key.get_pressed()
+    if keystate[K_RIGHT]:
+        bgsound.fadeout(5000)
+        beeX += 5
+    if keystate[K_LEFT]:
+        beeX -= 5
+    if keystate[K_UP]:
+        beeY -= 5
+    if keystate[K_DOWN]:
+        beeY += 5
 
-        screen.fill((0,180,0))
-        screen.blit(bee, (beeX, beeY))
-        pygame.display.flip()
+    screen.fill((0,180,0))
+    screen.blit(bee, (beeX, beeY))
+    pygame.display.flip()
             
-
-if __name__ == '__main__': 
-    main()
+pygame.quit()
