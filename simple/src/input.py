b6f0efd5e0df64b617554908d784790dd87c119e
daveb <daveb@davebsoft.com>
2012-06-17 17:06:54 -0700

    Add a second example of input, this time with an integer response

--- simple/src/input.py
+++ simple/src/input.py
@@ -1,4 +1,7 @@
-''' Shows an example of the input function '''
+''' Shows examples of the input function '''
 
 name = input('What is your name? ')
 print('Hello, ' + name)
+
+age = int(input('What is your age, in years? '))
+print('Twice your age is', age * 2)
