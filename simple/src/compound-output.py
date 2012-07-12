c6ad69611263d708be052c8b017737f1b0b4e650
daveb <daveb@davebsoft.com>
2012-07-11 23:00:43 -0700

    Compound output example

---
+++ simple/src/compound-output.py
@@ -0,0 +1,12 @@
+name = "Sue"
+age = 12
+score = 95
+
+print(name, age, score)
+print(name + str(age) + str(score))
+print(name + " " + str(age) + " " + str(score))
+print(name + ", " + str(age) + ", " + str(score))
+
+print(name + ", age", str(age) + ", scored", score)
+print(name + ", age", str(age) + ", scored " + str(score))
+print("%s, age %d, scored %x" % (name, age, score))
