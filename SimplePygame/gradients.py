61b2598f7fbdbf15fc8447aac003231a30eb8668
daveb <daveb@davebsoft.com>
2013-02-10 11:19:29 -0800

    Use all caps for constant names

--- SimplePygame/gradients.py
+++ SimplePygame/gradients.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame.locals import *
 
-gradient_height = 100
-margin_height = 2
+GRADIENT_HEIGHT = 100
+MARGIN_HEIGHT = 2
 
 def red(intensity):
     return (intensity, 0, 0)
@@ -15,20 +15,20 @@ def blue(intensity):
 
 def gradient(surface, y, color_fn):
     for i in range(256):
-        pygame.draw.line(surface, color_fn(i), (i, y), (i, y + gradient_height))
+        pygame.draw.line(surface, color_fn(i), (i, y), (i, y + GRADIENT_HEIGHT))
 
 color_fns = red, green, blue
 pygame.init()
 num_margins = len(color_fns) - 1
-screen = pygame.display.set_mode((256, gradient_height * len(color_fns) + 
-    num_margins * margin_height), 0)
+screen = pygame.display.set_mode((256, GRADIENT_HEIGHT * len(color_fns) + 
+    num_margins * MARGIN_HEIGHT), 0)
 screen.fill((255, 255, 255))
 pygame.display.set_caption('Gradients')
 y = 0
 
 for fn in color_fns:
     gradient(screen, y, fn)
-    y += gradient_height + margin_height
+    y += GRADIENT_HEIGHT + MARGIN_HEIGHT
 
 pygame.display.flip()
 
