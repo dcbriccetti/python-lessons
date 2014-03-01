21e8b488305d90c0e2bfcf93f19bf439b83b1adc
Dave Briccetti <daveb@davebsoft.com>
2014-02-28 22:20:59 -0800

    Make a few simple improvements

--- SimplePygame/text.py
+++ SimplePygame/text.py
@@ -1,23 +1,23 @@
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, Color
 
 pygame.init()
-screen = pygame.display.set_mode((200, 50), 0)
+screen = pygame.display.set_mode((620, 115), 0)
 pygame.display.set_caption('Text Drawing Example')
 
-font = pygame.font.Font(None, 20)
-image = font.render('Hi there', 0, Color('green'))
+font = pygame.font.Font(None, 150)
+image = font.render('Hello, world', True, Color('green'))
 clock = pygame.time.Clock()
+black = Color('black')
 
 loop = True
 while loop:
     for event in pygame.event.get():
-        if event.type == QUIT \
-            or (event.type == KEYDOWN and event.key == K_ESCAPE):
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             loop = False
 
-    screen.fill((0, 0, 0))
-    screen.blit(image, (4, 4))
+    screen.fill(black)
+    screen.blit(image, (8, 8))
     pygame.display.flip()
     clock.tick(10)
 
