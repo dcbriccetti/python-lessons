229e60d5fbc040c7d7ca1ce0804a3680aae7455f
Dave Briccetti <daveb@davebsoft.com>
2014-02-28 22:37:22 -0800

    Simplify by removing the loop and event handling

--- SimplePygame/text.py
+++ SimplePygame/text.py
@@ -1,24 +1,16 @@
+from time import sleep
 import pygame
-from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, Color
+from pygame.locals import Color
 
 pygame.init()
 screen = pygame.display.set_mode((620, 115), 0)
 pygame.display.set_caption('Text Drawing Example')
-
 font = pygame.font.Font(None, 150)
 image = font.render('Hello, world', True, Color('green'))
 clock = pygame.time.Clock()
 black = Color('black')
-
-loop = True
-while loop:
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            loop = False
-
-    screen.fill(black)
-    screen.blit(image, (8, 8))
-    pygame.display.flip()
-    clock.tick(10)
-
+screen.fill(black)
+screen.blit(image, (8, 8))
+pygame.display.flip()
+sleep(3)
 pygame.quit()
