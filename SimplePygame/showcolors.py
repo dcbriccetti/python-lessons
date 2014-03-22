35c924339c4a7803c30bcbb37f316c47d1f61468
Dave Briccetti <daveb@davebsoft.com>
2014-03-21 17:15:20 -0700

    Show all the predefined Pygame colors

---
+++ SimplePygame/showcolors.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.constants import KEYDOWN, K_UP, K_DOWN, K_ESCAPE, QUIT
+
+pygame.init()
+screen = pygame.display.set_mode((200, 100))
+clock = pygame.time.Clock()
+framesPerSec = 10
+run = True
+
+for name, rgb in pygame.color.THECOLORS.iteritems():
+    if not run:
+        break
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            run = False
+        elif event.type == KEYDOWN:
+            if event.key == K_DOWN and framesPerSec > 1:
+                framesPerSec -= 1
+            elif event.key == K_UP:
+                framesPerSec += 1
+
+    screen.fill(rgb)
+    pygame.display.set_caption(name)
+    pygame.display.flip()
+    clock.tick(framesPerSec)
+
+pygame.quit()
