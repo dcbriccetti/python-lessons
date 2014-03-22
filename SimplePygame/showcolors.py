9e374fc28cea1f2477bd8ded721e2fa4844c9a8a
Dave Briccetti <daveb@davebsoft.com>
2014-03-21 18:06:04 -0700

    Turn on keyboard repeat. Move “break” to better location.

--- SimplePygame/showcolors.py
+++ SimplePygame/showcolors.py
@@ -2,14 +2,13 @@ import pygame
 from pygame.constants import KEYDOWN, K_UP, K_DOWN, K_ESCAPE, QUIT
 
 pygame.init()
+pygame.key.set_repeat(100, 50)
 screen = pygame.display.set_mode((200, 100))
 clock = pygame.time.Clock()
 framesPerSec = 10
 run = True
 
 for name, rgb in pygame.color.THECOLORS.iteritems():
-    if not run:
-        break
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             run = False
@@ -18,6 +17,9 @@ for name, rgb in pygame.color.THECOLORS.iteritems():
                 framesPerSec -= 1
             elif event.key == K_UP:
                 framesPerSec += 1
+
+    if not run:
+        break
 
     screen.fill(rgb)
     pygame.display.set_caption(name)
