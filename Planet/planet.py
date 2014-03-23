e29c7856299cbcced3a5fca0b2395529e415ae41
Dave Briccetti <daveb@davebsoft.com>
2014-03-22 17:57:41 -0700

    Add a star field

--- Planet/planet.py
+++ Planet/planet.py
@@ -1,25 +1,29 @@
 import pygame
 from pygame.constants import FULLSCREEN, KEYDOWN, K_ESCAPE, QUIT
+from stars import draw_field
 
 pg = pygame
 pd = pg.display
 pg.init()
 screen = pd.set_mode((0, 0), FULLSCREEN)
-screen_width = screen.get_rect().width
+sr = screen.get_rect()
 planet = pg.image.load('redplanet.png')
+star_field = screen.copy()
+draw_field(star_field)
+
 clock = pygame.time.Clock()
 run = True
 
-for planet_size in range(0, screen_width, 2):
+for planet_size in range(0, sr.width, 2):
     for event in pg.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             run = False
     if not run:
         break
     planet_scaled = pg.transform.scale(planet, (planet_size, planet_size))
-    screen.fill((0, 0, 0))
-    x = screen_width / 2 - planet_size / 2
-    y = screen.get_rect().height / 2 - planet_size / 2
+    x = sr.width / 2 - planet_size / 2
+    y = sr.height / 2 - planet_size / 2
+    screen.blit(star_field, (0, 0))
     screen.blit(planet_scaled, (x, y))
     pd.flip()
     clock.tick(40)
---
+++ Planet/stars.py
@@ -0,0 +1,22 @@
+import pygame
+from random import gauss, randint
+
+pg = pygame
+pd = pg.display
+
+def draw_field(star_field):
+    sr = star_field.get_rect()
+    star_field.fill((0, 0, 0))
+    for n in range(int(gauss(500, 100))):
+        gray = randint(1, 255)
+        bluer = redder = yellower = greener = 0
+        ri = randint(1, 10)
+        if ri == 1:
+            bluer = randint(0, 255 - gray)
+        elif ri == 2:
+            redder = randint(0, 255 - gray)
+        elif ri == 3:
+            yellower = randint(0, 255 - gray)
+            redder = greener = yellower
+        pg.draw.circle(star_field, (gray + redder, gray + greener, gray + bluer), \
+                       (randint(0, sr.width), randint(0, sr.height)), max(0, int(gauss(1, 1))))
