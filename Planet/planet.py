1edfea0b11cfa01fc6af4afb104ab7066d4cc5b4
Dave Briccetti <daveb@davebsoft.com>
2014-03-22 18:43:06 -0700

    Improve performance. Add move in, out keys.

--- Planet/planet.py
+++ Planet/planet.py
@@ -1,10 +1,11 @@
 import pygame
-from pygame.constants import FULLSCREEN, KEYDOWN, K_ESCAPE, QUIT
+from pygame.constants import FULLSCREEN, KEYDOWN, K_ESCAPE, K_UP, K_DOWN, QUIT
 from stars import draw_field
 
 pg = pygame
 pd = pg.display
 pg.init()
+pg.key.set_repeat(1, 20)
 screen = pd.set_mode((0, 0), FULLSCREEN)
 sr = screen.get_rect()
 planet = pg.image.load('redplanet.png')
@@ -14,20 +15,33 @@ draw_field(star_field)
 clock = pygame.time.Clock()
 run = True
 
-for planet_size in range(0, sr.width, 2):
+planet_size = sr.width / 20
+planet_size_increment = max(5, sr.width / 500)
+image_changed = True
+
+while True:
     for event in pg.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             run = False
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                planet_size += planet_size_increment
+                image_changed = True
+            elif event.key == K_DOWN and planet_size - planet_size_increment >= 1:
+                planet_size -= planet_size_increment
+                image_changed = True
     if not run:
         break
-    planet_scaled = pg.transform.scale(planet, (planet_size, planet_size))
-    x = sr.width / 2 - planet_size / 2
-    y = sr.height / 2 - planet_size / 2
-    screen.blit(star_field, (0, 0))
-    screen.blit(planet_scaled, (x, y))
-    pd.flip()
-    clock.tick(40)
 
-pg.time.wait(200)
+    if image_changed:
+        screen.blit(star_field, (0, 0))
+        planet_scaled = pg.transform.scale(planet, (planet_size, planet_size))
+        x = sr.centerx - planet_size / 2
+        y = sr.centery - planet_size / 2
+        screen.blit(planet_scaled, (x, y))
+        pd.flip()
+        image_changed = False
+
+    clock.tick(100)
 
 pg.quit()
