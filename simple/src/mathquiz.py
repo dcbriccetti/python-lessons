ddcd72ad5115d0be8bff6755f1278c0b5ce57ab5
daveb <daveb@davebsoft.com>
2014-07-02 11:23:13 -0700

    Add timing of response

--- simple/src/mathquiz.py
+++ simple/src/mathquiz.py
@@ -1,12 +1,18 @@
 from random import randint
+from time import time
 
 print('Enjoy a math quiz, and respond 0 to stop.')
 
 while True:
     m1 = randint(2, 11)
     m2 = randint(2, 11)
-    prod = m1 * m2
-    ans = int(input('What is {0} * {1} '.format(m1, m2)))
-    if ans == 0:
+    product = m1 * m2
+    start_time = time()
+    answer = int(input('What is %d * %d ' % (m1, m2)))
+    if answer == 0:
         break
-    print('Right' if ans == prod else 'Wrong, the answer was %d' % prod)
+        
+    if answer == product:
+        print('Right, in %.2f seconds' % (time() - start_time))
+    else:
+        print('Wrong, the answer was %d' % product)
