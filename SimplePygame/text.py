ce9040ba32c72aff83d55f7d8428422d3e45a86d
Dave Briccetti <daveb@davebsoft.com>
2014-07-08 17:36:55 -0700

    Inline a variable

--- SimplePygame/text.py
+++ SimplePygame/text.py
@@ -4,12 +4,10 @@ from pygame.locals import Color
 
 pygame.init()
 screen = pygame.display.set_mode((620, 115), 0)
-pygame.display.set_caption('Text Drawing Example')
 font = pygame.font.Font(None, 150)
 image = font.render('Hello, world', True, Color('green'))
 clock = pygame.time.Clock()
-black = Color('black')
-screen.fill(black)
+screen.fill(Color('black'))
 screen.blit(image, (8, 8))
 pygame.display.flip()
 sleep(3)
