97e52c4d46ff210ec109a7e07734d041f5110be2
Dave Briccetti <daveb@davebsoft.com>
2014-07-22 12:09:37 -0700

    Revert to simpler version

--- SimplePygame/shapes.py
+++ SimplePygame/shapes.py
@@ -1,26 +1,16 @@
 import pygame
-from time import sleep
-from random import randint
 from pygame.locals import *
 
-def rc():
-    return randint(0, 255)
-
 pygame.init()
-screen = pygame.display.set_mode((400, 400), 0)
+screen = pygame.display.set_mode((400, 300), 0)
+screen.fill(Color('black'))
+pygame.draw.rect(screen, Color('red'), Rect(10, 10, 100, 100))
+pygame.draw.circle(screen, Color('blue'), (200, 110), 50)
+pygame.display.flip()
 
 loop = True
 while loop:
-    screen.fill(Color('black'))
-    for x in range(0, 400, 50):
-        for y in range(0, 400, 50):
-            pygame.draw.rect(screen,
-                Color(rc(), rc(), rc()), Rect(x + 5, y + 5, 40, 40))
-
-    pygame.display.flip()
-    sleep(.1)
-
-    event = pygame.event.poll()
+    event = pygame.event.wait()
     if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
         loop = False
 
