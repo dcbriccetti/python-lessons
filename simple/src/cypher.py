19c737d61a6b161a4c48f07afc915ba88cdb93db
Dave Briccetti <daveb@davebsoft.com>
2015-06-18 13:19:49 -0700

    Refactor to clarify

--- simple/src/cypher.py
+++ simple/src/cypher.py
@@ -2,11 +2,16 @@ plaintext   = 'meet me at the usual place'
 fromLetters = 'abcdefghijklmnopqrstuv0123456789 '
 toLetters   = '6n4pde3fs 2c1ivjr05lq8utbgam7hk9o'
 
-for c in plaintext:
-    print(toLetters[fromLetters.index(c)], end='')
+for plaintext_char in plaintext:
+    from_letters_index = fromLetters.index(plaintext_char)
+    encrypted_letter = toLetters[from_letters_index]
+    s = "%s -> %s" % (plaintext_char, encrypted_letter)
+    print(s)
 
 print()
     
 cyphertext = '1ddlo1do6lolfdoq5q6cojc64d'
-for c in cyphertext:
-    print(fromLetters[toLetters.index(c)], end='')
+for cyphertext_char in cyphertext:
+    to_letters_index = toLetters.index(cyphertext_char)
+    decrypted_letter = fromLetters[to_letters_index]
+    print(decrypted_letter, end='')
