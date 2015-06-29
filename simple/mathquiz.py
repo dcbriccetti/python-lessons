c494cc8360623c3a94b432357c423162c84a3297
Dave Briccetti <daveb@davebsoft.com>
2015-06-29 13:59:00 -0700

    Improve mathquiz by not failing on an empty response

--- simple/mathquiz.py
+++ simple/mathquiz.py
@@ -1,17 +1,18 @@
 from random import randint
 from time import time
 
-print('Enjoy a math quiz, and respond 0 to stop.')
+print('Enjoy a math quiz, and respond with nothing to stop.')
 
 while True:
     m1 = randint(2, 11)
     m2 = randint(2, 11)
     product = m1 * m2
     start_time = time()
-    answer = int(input('What is %d * %d ' % (m1, m2)))
-    if answer == 0:
+    response = input('What is %d * %d ' % (m1, m2))
+    if response == '':
         break
-        
+    answer = int(response)
+
     if answer == product:
         print('Right, in %.2f seconds' % (time() - start_time))
     else:
