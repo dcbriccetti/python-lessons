a3d14110d6e13f95d127659ddc6152f9b5a35d69
Dave Briccetti <daveb@davebsoft.com>
2015-06-29 14:54:56 -0700

    Improve functions example

--- simple/functions1.py
+++ simple/functions1.py
@@ -1,12 +1,15 @@
 def letter_grade(score):
-    if score >= 90:
-        grade = 'A'
-    else:
+    if score < 60:
+        grade = 'F'
+    elif score < 70:
+        grade = 'D'
+    elif score < 80:
+        grade = 'C'
+    elif score < 90:
         grade = 'B'
+    else:
+        grade = 'A'
     return grade
 
-while True:
-    score = int(input('Score? '))
-    if score < 0:
-        break
-    print(letter_grade(score))
+for score in range(55, 101):
+    print(score, letter_grade(score))
