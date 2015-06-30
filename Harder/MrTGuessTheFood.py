b30276f35793fafa843ea78d03affedff33c1c14
Dave Briccetti <daveb@davebsoft.com>
2015-06-29 21:09:19 -0700

    Reformat code

--- Harder/MrTGuessTheFood.py
+++ Harder/MrTGuessTheFood.py
@@ -1,4 +1,5 @@
 from random import choice
+
 items = ('soup', 'corn', 'milk', 'bread', 'eggs')
 item = choice(items)
 positiveSayings = ('Stay in school!', "Don't do drugs!")
@@ -8,13 +9,13 @@ print('Mr. T says guess the food item in', guessesLeft, 'guesses.')
 print('Items:', items)
 
 while guessesLeft > 0:
-	guessesLeft -= 1
-	guess = input('Your guess? ')
-	if guess == item:
-		print("That's right. " + choice(positiveSayings))
-		break # Exit the while loop
-	if guessesLeft:
-		print('Wrong.', guessesLeft, 'guesses left.')
+    guessesLeft -= 1
+    guess = input('Your guess? ')
+    if guess == item:
+        print("That's right. " + choice(positiveSayings))
+        break  # Exit the while loop
+    if guessesLeft:
+        print('Wrong.', guessesLeft, 'guesses left.')
 
 if guess != item:
-	print("I pity the fool!")
+    print("I pity the fool!")
