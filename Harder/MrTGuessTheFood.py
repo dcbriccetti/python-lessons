528048ab1d98a7d8eec782f78d320549320ae216
Dave Briccetti <daveb@davebsoft.com>
2015-06-29 21:25:35 -0700

    Use a for loop instead of while

--- Harder/MrTGuessTheFood.py
+++ Harder/MrTGuessTheFood.py
@@ -3,19 +3,18 @@ from random import choice
 items = ('soup', 'corn', 'milk', 'bread', 'eggs')
 item = choice(items)
 positiveSayings = ('Stay in school!', "Don't do drugs!")
-guessesLeft = 3
 
-print('Mr. T says guess the food item in', guessesLeft, 'guesses.')
-print('Items:', items)
+MAX_GUESSES = 3
+print('Mr. T says guess the food item in %d guesses.' % MAX_GUESSES)
+print('Items: ' + ', '.join(items))
 
-while guessesLeft > 0:
-    guessesLeft -= 1
+for guessNum in range(MAX_GUESSES):
     guess = input('Your guess? ')
     if guess == item:
         print("That's right. " + choice(positiveSayings))
         break  # Exit the while loop
-    if guessesLeft:
-        print('Wrong.', guessesLeft, 'guesses left.')
+
+    print('Wrong.', MAX_GUESSES - guessNum - 1, 'guesses left.')
 
 if guess != item:
     print("I pity the fool!")
