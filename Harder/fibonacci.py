434521e957ba7444553b11a06d4ebb624a1d6651
Dave Briccetti <daveb@davebsoft.com>
2015-10-02 15:31:50 -0700

    Simplify fibonacci program

--- Harder/fibonacci.py
+++ Harder/fibonacci.py
@@ -1,10 +1,7 @@
 num_to_make = 10
 
 num1, num2 = 0, 1
-print(num1)
-print(num2)
 
-for n in range(num_to_make - 2):
+for n in range(num_to_make):
+    print(num1)
     num1, num2 = num2, num1 + num2
-    print(num2)
-
