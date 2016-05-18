f901c4391d0fa769b266ef7c557816660760e9b8
Dave Briccetti <daveb@davebsoft.com>
2016-05-18 15:17:09 -0700

    Make minor improvements

--- Harder/auto-high-low.py
+++ Harder/auto-high-low.py
@@ -4,12 +4,11 @@ from math import log2, pow
 
 class AiGuesser:
 	def __init__(self, highest, smart=False, chatty=True):
+		self.low = 1
 		self.high = self.highest = highest
 		self.smart = smart
 		self.chatty = chatty
-		self.low = 1
 		self.last_guess = None
-		self.guess_type = 'smart' if self.smart else 'random'
 
 	def guess(self, too_high):
 		if too_high is not None:
@@ -32,11 +31,15 @@ class AiGuesser:
 
 with open('high-low-results.tsv', 'w') as results:
 	results.write('Highest\tStrategy\tTrial\tGuesses\n')
+
 	for power in (1, 2, 3, 6, 9, 12):
 		highest = int(pow(10, power))
-		print('Between 1 and {0:,} (binary logarithm of {0:,} is {1:.2f}).'.format(highest, log2(highest)))
+		print('Between 1 and {0:,} (binary logarithm of {0:,} is {1:.2f}).'.format(
+			highest, log2(highest)))
+
 		for smart in (True, False):
 			print('Smart' if smart else 'Random')
+
 			for trial in range(1, 101):
 				print('Trial', trial)
 				guesser = AiGuesser(highest, smart=smart, chatty=True)
@@ -49,4 +52,5 @@ with open('high-low-results.tsv', 'w') as results:
 					guess = guesser.guess(guess > number)
 
 				guesser.win(guesses)
-				results.write('{}\t{}\t{}\t{}\n'.format(highest, 'Halving' if smart else 'Random', trial, guesses))
+				results.write('{}\t{}\t{}\t{}\n'.format(
+					highest, 'Halving' if smart else 'Random', trial, guesses))
