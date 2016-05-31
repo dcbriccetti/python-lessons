7547f977535480127382c3d9bfc82bc3dc829af4
Dave Briccetti <daveb@davebsoft.com>
2016-05-31 11:11:44 -0700

    Update name picker webapp

--- Harder/picker.py
+++ Harder/picker.py
@@ -3,10 +3,10 @@
 from random import randint
 import time
 
-names = ['sue', 'mary', 'hari', 'fred', 'eric', 'menlo']
+remaining_names = ['sue', 'mary', 'hari', 'fred', 'eric', 'menlo']
 
-while len(names) > 0:
-    pop_index = randint(0, len(names) - 1)
-    popped_name = names.pop(pop_index)
+while len(remaining_names) > 0:
+    pop_index = randint(0, len(remaining_names) - 1)
+    popped_name = remaining_names.pop(pop_index)
     print(popped_name)
     time.sleep(1)
---
+++ Web/name-picker/name_picker.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+from random import randint
+
+HOW_MANY_OF_EACH_NAME = 1
+all_names = ('Jim', 'Sue', 'Steve') * HOW_MANY_OF_EACH_NAME
+remaining_names = list(all_names)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if remaining_names:
+        name = remaining_names.pop(randint(0, len(remaining_names) - 1))
+        return render_template("names.html", name=name, remaining=len(remaining_names))
+    else:
+        return redirect("/reset")
+
+
+@app.route("/reset")
+def reset():
+    global remaining_names
+    remaining_names = list(all_names)
+    return redirect("/")
+
+app.run(debug=True)
