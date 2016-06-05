4f97069e8be2b5e0f5fb2d41eed9cf113649564b
Dave Briccetti <daveb@davebsoft.com>
2016-06-05 15:59:33 -0700

    Update google maps flask example

--- Web/flask/gmap/gmap.py
+++ Web/flask/gmap/gmap.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+school_codes = {
+    'hv': 'Happy Valley',
+    'stanley': 'Stanley'
+}
 
 @app.route("/")
-@app.route("/stanley")
 def index():
-    return render_template('map.html', lat=37.8862125, lng=-122.1148579)
+    return "Try /stanley or /hv"
+
+@app.route("/<school_code>")
+def school(school_code):
+    if school_code == 'stanley':
+        return render_template('map.html', lat=37.8862125, lng=-122.1148579, school=school_codes[school_code])
+    elif school_code == 'hv':
+        return render_template('map.html', lat=37.905, lng=-122.1445, school=school_codes[school_code])
 
 app.run(host='0.0.0.0', debug=True)
