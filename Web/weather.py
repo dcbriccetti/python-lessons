6ecfbf80d4649a0b99e18eef17b0f9beae379897
Dave Briccetti <daveb@davebsoft.com>
2016-06-05 00:21:59 -0700

    Simplify weather program

--- Web/weather.py
+++ Web/weather.py
@@ -3,17 +3,9 @@ import requests
 key = open('openweathermap.txt').read().strip()  # Put your API key in this file
 
 
-def kelvin_to_celcius(k):
-    return k - 273.15
-
-
 def temp_at_location(location):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&mode=json&APPID=%s' % (location, key)
-    resp = requests.get(url)
-    json = resp.json()
-    main = json['main']
-    kelvin_temp = main['temp']
-    celcius_temp = kelvin_to_celcius(kelvin_temp)
-    return celcius_temp
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&mode=json&units=metric&APPID=%s' % (location, key)
+    json = requests.get(url).json()
+    return json['main']['temp']
 
 print('%.1f' % temp_at_location('94549,US'))
