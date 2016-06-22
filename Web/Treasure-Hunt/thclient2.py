a038e4460c41705aa509666318bce9e4cdbea609
Dave Briccetti <daveb@davebsoft.com>
2016-06-22 15:58:52 -0700

    Update text processing notebook; advanced treasure hunt client and server

--- Web/Treasure-Hunt/thclient2.py
+++ Web/Treasure-Hunt/thclient2.py
@@ -7,40 +7,41 @@ clue_loc = url_base
 
 
 def solve_largest_puzzle(text):
-	open_paren = text.index('(')
-	close_paren = text.index(')')
-	nums_str = text[open_paren + 1:close_paren]
-	nums = [int(n) for n in nums_str.split(',')]
-	return url_base + str(max(nums))
+    open_paren = text.index('(')
+    close_paren = text.index(')')
+    nums_str = text[open_paren + 1:close_paren]
+    nums = [int(n) for n in nums_str.split(', ')]
+    return url_base + str(max(nums))
 
 
 def solve_multiply_puzzle():
-	m1 = int(requests.get(url_base + 'm1').text)
-	m2 = int(requests.get(url_base + 'm2').text)
-	return url_base + str(m1 * m2)
+    m1 = int(requests.get(url_base + 'multiplier').text)
+    m2 = int(requests.get(url_base + 'multiplicand').text)
+    return url_base + str(m1 * m2)
 
 
 def solve_three_vowels_puzzle(text):
-	open_paren = text.index('(')
-	close_paren = text.index(')')
-	words = text[open_paren + 1:close_paren].split(',')
-	r = re.compile('.*[aeiou]{3}.*')
-	matching = [word for word in words if r.search(word)]
-	return url_base + matching[0]
+    open_paren = text.index('(')
+    close_paren = text.index(')')
+    words = text[open_paren + 1:close_paren].split(', ')
+    r = re.compile('.*[aeiou]{3}.*')
+    matching = [word for word in words if r.search(word)]
+    return url_base + matching[0]
+
 
 while not treasure_found:
-	resp = requests.get(clue_loc)
-	text = resp.text
-	print(text)
+    resp = requests.get(clue_loc)
+    text = resp.text
+    print(text)
 
-	if text == 'You have reached the treasure!':
-		treasure_found = True
-	else:
-		if 'where number is the largest' in text:
-			clue_loc = solve_largest_puzzle(text)
-		elif 'm1 * http' in text:
-			clue_loc = solve_multiply_puzzle()
-		elif 'three vowels in a row' in text:
-			clue_loc = solve_three_vowels_puzzle(text)
-		else:
-			clue_loc = text
+    if text == 'You have reached the treasure!':
+        treasure_found = True
+    else:
+        if 'where <number> is the largest' in text:
+            clue_loc = solve_largest_puzzle(text)
+        elif '×' in text:
+            clue_loc = solve_multiply_puzzle()
+        elif 'three vowels in a row' in text:
+            clue_loc = solve_three_vowels_puzzle(text)
+        else:
+            clue_loc = text
--- Web/Treasure-Hunt/thserver2.py
+++ Web/Treasure-Hunt/thserver2.py
@@ -1,48 +1,58 @@
-from random import randint
-from flask import Flask
+import string
+from random import randint, choice
+from flask import Flask, abort
 app = Flask(__name__)
 
-url_base = "http://192.168.1.7:5000/"
-mlt1 = randint(1, 10)
-mlt2 = randint(1, 10)
-product = mlt1 * mlt2
+URL_BASE = "http://localhost:5000/"
+
+
+def path_gen():
+    chars = string.ascii_lowercase + string.digits
+    while True:
+        yield ''.join([choice(chars) for a in range(5)])
+
+multiplier = randint(1, 10)
+multiplicand = randint(1, 10)
+actual_product = multiplier * multiplicand
+rg = path_gen()
+max_puzzle_path = next(rg)
 treasure = str(1000 + randint(0, 999))
-max_puzzle_nums = str(2000 + randint(0, 999))
 max_of = [3000 + randint(0, 999) for n in range(500)]
 largest = str(max(max_of))
 max_of_str = [str(n) for n in max_of]
 
 
-def u(p): return url_base + p
+def u(p): return URL_BASE + p
+
 
 @app.route("/")
 def start():
-    return "%s where number = %s * %s" % (u("number"), u("m1"), u("m2"))
+    return "%s where <number> = %s × %s" % (u("<number>"), u('multiplier'), u('multiplicand'))
 
 
-@app.route("/m1")
+@app.route('/multiplier')
 def m1():
-    return str(mlt1)
+    return str(multiplier)
 
 
-@app.route("/m2")
+@app.route('/multiplicand')
 def m2():
-    return str(mlt2)
+    return str(multiplicand)
 
 
-@app.route("/" + str(product))
-def product():
-    return u(max_puzzle_nums)
-
-
-@app.route("/" + max_puzzle_nums)
+@app.route("/" + max_puzzle_path)
 def max_puzzle():
-    return "%s where number is the largest of (%s)" % (u("number"), ",".join(max_of_str))
+    return "%s where <number> is the largest of (%s)" % (u('<number>'), ', '.join(max_of_str))
+
+
+@app.route("/<int:product_guess>")
+def product(product_guess):
+    return u(max_puzzle_path) if product_guess == actual_product else abort(404)
 
 
 @app.route("/" + largest)
 def words_matching_regex():
-    return '%s where string is the word from this list with three vowels in a row: (kidnapper,bindweed,euonymus,almost,marabou,dismembered,underused,struma,assumptions,tzar,naze,wore,bare,jones,stick,zebrine,laxative,reynard,uninjured,river,manners,faulty,neglect,required,shabby,lobster,dour,fraise,colognes,expressing,eardrop,sneaker,adultery)' % \
+    return '%s where string is the word from this list with three vowels in a row: (kidnapper, bindweed, euonymus, almost, marabou, dismembered, underused, struma, assumptions, tzar, naze, wore, bare, jones, stick, zebrine, laxative, reynard, uninjured, river, manners, faulty, neglect, required, shabby, lobster, dour, fraise, colognes, expressing, eardrop, sneaker, adultery)' % \
         u("string")
 
 
