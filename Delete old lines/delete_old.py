5b546ee8eaf8bbfaf53a5e3eeba96312f7f99be1
Dave Briccetti <daveb@davebsoft.com>
2016-06-22 13:48:39 -0700

    Delete unused

--- Delete old lines/delete_old.py
+++
@@ -1,9 +0,0 @@
-with open('data') as file:
-    lines_read = 0
-    while True:
-        line = file.readline()
-        lines_read += 1
-        assert(line)
-        if line.startswith('COPY "Table A"'):
-            break
-print('%d lines read' % lines_read)
