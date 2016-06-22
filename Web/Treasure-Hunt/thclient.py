1eac2fb9781a51cfb0019764086387cb02a19633
Dave Briccetti <daveb@davebsoft.com>
2016-06-22 14:05:30 -0700

    Update simple treasure hunt client and server

--- Web/Treasure-Hunt/thclient.py
+++ Web/Treasure-Hunt/thclient.py
@@ -1,12 +1,15 @@
 import requests
 
 treasure_found = False
-clue_loc = 'http://localhost:5000/n-2'
+URL_START = 'http://localhost:5000'
+clue_loc = '/'
 
 while not treasure_found:
-	resp = requests.get(clue_loc)
-	print(resp.text)
-	if resp.text == 'You have reached the treasure!':
-		treasure_found = True
-	else:
-		clue_loc = resp.text
+    print('Going to', clue_loc)
+    resp = requests.get(URL_START + clue_loc)
+    if resp.text == 'You have reached the treasure!':
+        treasure_found = True
+        print(resp.text)
+    else:
+        print('Clue from %s: %s' % (clue_loc, resp.text))
+        clue_loc = resp.text
--- Web/Treasure-Hunt/thserver.py
+++ Web/Treasure-Hunt/thserver.py
@@ -1,20 +1,30 @@
+'''
+A simple treasure hunt server.
+/ -> /garden -> /shed -> /attic
+'''
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route('/')
+def start():
+    return '/garden'
+
+
+@app.route('/garden')
+def garden():
+    return '/shed'
+
+
+@app.route('/shed')
+def shed():
+    return '/attic'
+
+
+@app.route('/attic')
+def attic():
     return "You have reached the treasure!"
 
-
-@app.route("/n-1")
-def n1():
-    return "http://localhost:5000/"
-
-
-@app.route("/n-2")
-def n2():
-    return "http://localhost:5000/n-1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
