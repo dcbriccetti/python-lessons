48d97150efab45a175aac1515ff69c133b262c86
Dave Briccetti <daveb@davebsoft.com>
2016-07-04 22:01:49 -0700

    Update gmap (Google Map) program to use Bootstrap, better data structures.

--- Web/flask/gmap/gmap.py
+++ Web/flask/gmap/gmap.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, abort
 app = Flask(__name__)
 
-schools = {
-    'hv':       ('Happy Valley',    37.905,     -122.1445),
-    'stanley':  ('Stanley',         37.8862125, -122.1148579)
-}
+
+class School:
+    def __init__(self, key, name, lat, lng):
+        self.key  = key
+        self.name = name
+        self.lat  = lat
+        self.lng  = lng
+
+schools = (
+    School('hv',      'Happy Valley Elementary', 37.905, -122.1445),
+    School('stanley', 'Stanley Middle',          37.886, -122.1149)
+)
+schools_by_key = {school.key: school for school in schools}
 
 
 @app.route("/")
 def index():
-    return "Try /stanley or /hv"
+    return render_template('index.html', schools=schools)
 
 
 @app.route("/<school_code>")
 def show_school(school_code):
-    school = schools.get(school_code)
+    school = schools_by_key.get(school_code)
     if school:
-        return render_template('map.html', lat=school[1], lng=school[2], school=school[0])
+        return render_template('map.html', school=school)
     else:
         abort(404)
 
