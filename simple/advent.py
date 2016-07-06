738cf6763a239293fcd9a8b5ae0e537603f90104
Dave Briccetti <daveb@davebsoft.com>
2016-07-06 16:55:32 -0700

    Change to avoid sys.exit (for Jupyter Notebook)

--- simple/advent.py
+++ simple/advent.py
@@ -1,32 +1,34 @@
 from random import choice, randint
-from sys import exit
 
 transitions = {
     'airstrip': ('forest',),
-    'forest': ('airstrip', 'cave'),
-    'cave': ('forest', 'meadow'),
-    'meadow': ('cave',),
+    'forest':   ('airstrip', 'cave'),
+    'cave':     ('forest', 'meadow'),
+    'meadow':   ('cave',),
 }
 places = tuple(transitions.keys())
 place = choice(places)
+alive = True
 
-while True:
+while alive:
     print('You are at the', place)
     destinations = transitions[place]
     print('From here you can go to', ', '.join(destinations))
-    newDest = input('Where would you like to go? ')
-    if newDest == 'quit':
+    new_place = input('Where would you like to go? ')
+    if new_place == 'quit':
         break
-    if newDest in destinations:
-        if randint(1, 10) == 1:
-            print('You were struck by lightning. Game over.')
-            exit()
-        place = newDest
-        if place == 'meadow':
-            if randint(1, 2) == 1:
-                print('You fell in a hole and you are stuck.')
-                exit()
-            if randint(1, 2) == 1:
-                print('You discover an amazing treasure!')
     else:
-        print("You can't go there.")
+        if new_place in destinations:
+            if randint(1, 10) == 1:
+                print('You were struck by lightning. Game over.')
+                alive = False
+            else:
+                place = new_place
+                if place == 'meadow':
+                    if randint(1, 3) == 1:
+                        print('You fell in a hole and you are stuck.')
+                        alive = False
+                    elif randint(1, 3) == 1:
+                        print('You discover an amazing treasure!')
+        else:
+            print("You can't go there.")
