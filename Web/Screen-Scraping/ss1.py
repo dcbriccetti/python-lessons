37323356ee3fe6bf33a5c374236d46238142e9a7
Dave Briccetti <daveb@davebsoft.com>
2016-07-20 15:10:01 -0700

    Add screen scraping example

---
+++ Web/Screen-Scraping/ss1.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+html_doc = requests.get('https://www.google.com/search?q=mlb').text
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup.title.text)
