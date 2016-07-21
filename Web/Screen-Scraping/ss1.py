f41d0c6fe1f19138e50db33e04f9ab077d3876a1
Dave Briccetti <daveb@davebsoft.com>
2016-07-21 00:02:02 -0700

    Update screen scraper example

--- Web/Screen-Scraping/ss1.py
+++ Web/Screen-Scraping/ss1.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 html_doc = requests.get('https://www.google.com/search?q=mlb').text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup.title.text)
+print(soup.prettify())
