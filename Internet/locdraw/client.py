c9790ed96922e8406ff308e79c8445065e2ca2e2
Dave Briccetti <daveb@davebsoft.com>
2016-08-17 01:14:41 -0700

    Add a Queue for thread safety and to simplify. Let the client program spawn multiple client threads.

--- Internet/locdraw/client.py
+++ Internet/locdraw/client.py
@@ -1,20 +1,33 @@
-from random import randint
+from threading import Thread, current_thread, local
+from random import randint, random, choice
 from time import sleep
 from socket import socket, AF_INET, SOCK_STREAM
 from port import PORT
-
-socket = socket(AF_INET, SOCK_STREAM)
-socket.connect(('127.0.0.1', PORT))
 
 
 def rc():
     return randint(-300, 300)
 
-try:
-    for n in range(100):
-        socket.send(('Dave\t%i\t%i\tblue' % (rc(), rc())).encode())
-        sleep(2)
-except KeyboardInterrupt:
-    pass
 
-socket.close()
+colors = ('red', 'blue', 'yellow', 'brown', 'green', 'gray', 'purple')
+
+
+def draw(color):
+    tl = local()
+    tl.color = color
+    tl.socket = socket(AF_INET, SOCK_STREAM)
+    tl.socket.connect(('127.0.0.1', PORT))
+
+    name = 'client%f' % random()
+    try:
+        for i in range(100):
+            print('Socket file %i, %s sending' % (tl.socket.fileno(), current_thread().name))
+            tl.socket.send(('%s\t%i\t%i\t%s' % (name, rc(), rc(), tl.color)).encode())
+            sleep(0.5 + random())
+    except KeyboardInterrupt:
+        pass
+
+    tl.socket.close()
+
+for color in colors:
+    Thread(target=draw, args=(color,)).start()
--- Internet/locdraw/server.py
+++ Internet/locdraw/server.py
@@ -1,11 +1,12 @@
 import socket
+from queue import Queue
 import turtle
 from turtle import Turtle
-from threading import Thread, Event, current_thread
+from threading import Thread, current_thread
 from port import PORT
 
 serverSocket = None
-move_event = Event()
+cmd_queue = Queue()
 
 class Move:
     def __init__(self, parts):
@@ -14,19 +15,16 @@ class Move:
         self.y = int(parts[2])
         self.color = parts[3]
 
-move_cmd = None
 turtles = {}  # Client name -> turtle
 
 def handle_client(client_socket, address):
-    global move_cmd
     print('handle_client on ' + current_thread().name)
     client_socket.send("Hi there\n".encode())
     while True:
         line = client_socket.recv(1024).decode()
         parts = line.split('\t')
         if len(parts) == 4:
-            move_cmd = Move(parts)
-            move_event.set()
+            cmd_queue.put(Move(parts))
             print(address, line)
             client_socket.send('Thanks for that\n'.encode())
 
@@ -36,7 +34,7 @@ def listen_thread():
     print('listen_thread on ' + current_thread().name)
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.bind(('', PORT))
-    serverSocket.listen(1)
+    serverSocket.listen(100)
 
     while True:
         print('Game server waiting for connection')
@@ -46,16 +44,28 @@ def listen_thread():
 
 Thread(target=listen_thread).start()
 
+turtle_shapes = ('arrow', 'circle', 'square', 'triangle', 'classic', 'turtle')
+
+
+def add_turtle(name):
+    t = Turtle()
+    t.speed('fast')
+    t.penup()
+    t.shape(turtle_shapes[len(turtles) % len(turtle_shapes)])
+    turtles[name] = t
+    t.setheading(90)  # Point up
+    return t
+
 try:
-    turtle.setup(600, 600)
-    turtle.ht()
-    while True:
-        move_event.wait()
-        move_event.clear()
-        client_turtle = turtles.get(move_cmd.name, Turtle())
-        turtles[move_cmd.name] = client_turtle
-        client_turtle.pencolor(move_cmd.color)
-        client_turtle.goto(move_cmd.x, move_cmd.y)
+    turtle.setup(650, 650)
+    turtle.hideturtle()
+    for n in range(100000):
+        cmd = cmd_queue.get()
+        client_turtle = turtles.get(cmd.name) or add_turtle(cmd.name)
+        if n % 10 == 0:
+            turtle.clear()  # Todo clear the screen here–not working
+        client_turtle.pencolor(cmd.color)
+        client_turtle.goto(cmd.x, cmd.y)
 except KeyboardInterrupt:
     print('Stopping server')
     serverSocket.close()
