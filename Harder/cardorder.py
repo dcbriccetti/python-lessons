29a85c73745d728fa3fb38f5c91f575bd7d13245
Dave Briccetti <daveb@davebsoft.com>
2017-02-17 03:15:53 -0800

    Improve performance

--- Harder/cardorder.py
+++ Harder/cardorder.py
@@ -28,12 +28,14 @@ def correct_order(read_only_deck):
 
 def permutations():
     'Return a generator of deck tuples'
-    r = range(2, 11)
+    r = range(6, 11)  # We know where 1–5 are
     return ((1, c2, 2, c4, 3, c6, 4, c8, 5, c10)
-            for c2 in r for c4 in r for c6 in r for c8 in r for c10 in r)
+            for c2 in r for c4 in r for c6 in r for c8 in r for c10 in r
+            if len({c2, c4, c6, c8, c10}) == 5)  # All numbers must be different
 
 
 start = time()
 correct_deck = next(filter(correct_order, permutations()))
 assert correct_deck == (1, 6, 2, 10, 3, 7, 4, 9, 5, 8)
-print('Deck {} found in {:.3f} seconds. {:,} decks tried.'.format(correct_deck, time() - start, decks_tried))
+print('Deck {} found in {:,.2f} microseconds. {:,} decks tried.'.format(
+    correct_deck, (time() - start) * 1000000, decks_tried))
