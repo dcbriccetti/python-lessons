8b795cd7d0d1b0bcfa12834e1d1dea32f94bbf58
Dave Briccetti <daveb@davebsoft.com>
2017-04-13 17:17:38 -0700

    Add new runways program

---
+++ Harder/runways.py
@@ -0,0 +1,36 @@
+'''A starting point for doing some things with runway information'''
+
+from random import choice
+import csv
+
+
+class Runway:
+    def __init__(self, airport_id, runway_name):
+        self.airport_id = airport_id
+        self.runway = runway_name
+
+    def __str__(self):
+        return self.airport_id + ' ' + runway_name
+
+airports_by_id = {}
+
+# Data come from from http://ourairports.com/data/
+with open("airports.csv") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        airport_id = line[1]
+        airport_name = line[3]
+        airports_by_id[airport_id] = airport_name
+
+runways = []
+
+with open("runways.csv") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        airport_id = line[2]
+        runway_name = line[8]
+        runways.append(Runway(airport_id, runway_name))
+
+runway = choice(runways)
+airport = airports_by_id[runway.airport_id]
+print(airport, runway)
