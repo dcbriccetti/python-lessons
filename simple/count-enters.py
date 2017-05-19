2c0d0ef55a48f252d6e43070e1cdc86c5ccf1725
Dave Briccetti <daveb@davebsoft.com>
2017-05-18 18:47:11 -0700

    Add new program to count Enter presses

---
+++ simple/count-enters.py
@@ -0,0 +1,17 @@
+from time import time, sleep
+
+messages = ('How many times can you press enter in ten seconds?', 'Get ready', 3, 2, 1, 'Go!')
+
+for message in messages:
+    sleep(1)
+    print(message)
+
+start_time = time()
+end_time = start_time + 10
+presses = 0
+
+while time() < end_time:
+    input()
+    presses += 1
+
+print('You pressed enter %d times' % presses)
