f15190625f60a938aeedcfb0ea39be2e58c62a0e
Dave Briccetti <daveb@davebsoft.com>
2019-09-27 20:18:46 -0700

    Simplify code

--- images/graphpaper.py
+++ images/graphpaper.py
@@ -3,25 +3,23 @@
 from PIL import Image
 import numpy as np
 
-rows = 5000
-cols = 3000
-cols_center = cols // 2
+dims = (5000, 3000, 3)
+cols_center = dims[1] // 2
 grid_spacing = 20
 x_axis_spacing = 1000
 
-pixels = np.zeros((rows, cols, 3), dtype=np.uint8)
-pixels[:, :] = (255, 255, 255)
+pixels = np.full(dims, 255,  dtype=np.uint8)
 
 lc = 200
 line_color = (lc, lc, lc)
 axis_color = (0, 0, 0)
 
-for row in range(0, rows, grid_spacing):
+for row in range(0, dims[0], grid_spacing):
     pixels[row, :] = line_color
-for col in range(0, cols, grid_spacing):
+for col in range(0, dims[1], grid_spacing):
     pixels[:, col] = line_color
 pixels[:, cols_center - 1 : cols_center + 1] = axis_color
-for x_axis_row in range(x_axis_spacing // 2, rows, x_axis_spacing):
+for x_axis_row in range(x_axis_spacing // 2, dims[0], x_axis_spacing):
     pixels[x_axis_row - 1 : x_axis_row + 1] = axis_color
 
 img = Image.fromarray(pixels)
