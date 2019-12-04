8e3a4f41b2b6151a97f8c7e1d14f5ef446c4eccd
Dave Briccetti <daveb@davebsoft.com>
2019-12-03 20:21:16 -0800

    Modernize mathquiz

--- simple/mathquiz.py
+++ simple/mathquiz.py
@@ -4,16 +4,17 @@ from time import time
 print('Enjoy a math quiz, and respond with nothing to stop.')
 
 while True:
-    m1 = randint(2, 11)
-    m2 = randint(2, 11)
-    product = m1 * m2
+    num1 = randint(2, 9)
+    num2 = randint(11, 99)
+    product = num1 * num2
     start_time = time()
-    response = input('What is %d * %d ' % (m1, m2))
-    if not response.strip():
+    response = input(f'What is {num1} * {num2}? ')
+    if not response:
         break
-    answer = int(response)
 
-    if answer == product:
-        print('Right, in %.2f seconds' % (time() - start_time))
-    else:
-        print('Wrong, the answer was %d' % product)
+    try:
+        answer = int(response)
+        elapsed = time() - start_time
+        print(f'Right, in {elapsed:.2f} seconds' if answer == product else f'Wrong, the answer was {product}')
+    except ValueError:
+        print('You entered something other than an integer')
