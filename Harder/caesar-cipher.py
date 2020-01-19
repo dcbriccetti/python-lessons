c82fa6f1bce71da9d3d7d46324bc906e4089640f
Dave Briccetti <daveb@davebsoft.com>
2020-01-19 10:38:26 -0800

    Rewrite Caesar cypher

--- Harder/caesar-cipher.py
+++ Harder/caesar-cipher.py
@@ -1,12 +1,24 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz '
+simple_plaintext = 'The eagle flies at dawn.'
+all_ascii_chars: str = ''.join((chr(n) for n in range(256)))
 
-plaintext = 'the sparrow flies at midnight'
 
-def encrypt(plaintext):
-    ciphertext = ''
-    for char in plaintext:
-        index = (alphabet.index(char) + 1) % 27
-        ciphertext += alphabet[index]
-    return ciphertext
+def shifted_by(ch, shift_by):
+    return chr((ord(ch) + shift_by) % 256)
 
-print(encrypt(plaintext))
+
+def encrypt(plaintext) -> str:
+    return ''.join((shifted_by(ch, 1) for ch in plaintext))
+
+
+def decrypt(cyphertext) -> str:
+    return ''.join((shifted_by(ch, -1) for ch in cyphertext))
+
+
+if __name__ == '__main__':
+    # Test with a simple message, and with a string made of all 8-bit ASCII characters
+    for plaintext in (simple_plaintext, all_ascii_chars):
+        cyphertext: str = encrypt(plaintext)
+        print(cyphertext)
+        decoded: str = decrypt(cyphertext)
+        print(decoded)
+        assert plaintext == decoded
