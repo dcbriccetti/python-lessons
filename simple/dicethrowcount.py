3e3b3bad9446cf143bacc9b35cabd3ffa4722b6c
Dave Briccetti <daveb@davebsoft.com>
2020-07-02 13:22:55 -0700

    Update dice throw count program

--- simple/dicethrowcount.py
+++ simple/dicethrowcount.py
@@ -1,15 +1,15 @@
 from random import randint
 
-num_dice = 3
+num_dice = 2
 num_sides = 6
 num_throws = 10000
 list_len = num_dice * num_sides + 1
 
 counts = [0 for n in range(list_len)]
 
-for n in range(num_throws):
-	outcome = sum([randint(1, num_sides) for n in range(num_dice)])
+for i in range(num_throws):
+	outcome = sum([randint(1, num_sides) for j in range(num_dice)])
 	counts[outcome] += 1
 
 for outcome, count in enumerate(counts[num_dice: list_len]):
-	print("{:3,} {:6,}".format(outcome + num_dice, count))
+	print(f'{outcome + num_dice:3,}\t{count:6,}')
