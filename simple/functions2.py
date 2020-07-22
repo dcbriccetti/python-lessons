88f113d48df5c8a9dadb937997eaff277d50cddf
Dave Briccetti <daveb@davebsoft.com>
2020-07-22 12:56:02 -0700

    Update function example

--- simple/functions2.py
+++ simple/functions2.py
@@ -1,5 +1,6 @@
-def celToFahr(celciusTemp):
-    return celciusTemp * 1.8 + 32
+def celsius_to_fahrenheit(celsius_temp: float) -> float:
+    return celsius_temp * 1.8 + 32
 
-celciusTemp = int(input('What is the temperature in Celcius? '))
-print('In old fashioned Fahrenheit degrees, that is', celToFahr(celciusTemp))
+celsius_temp = float(input('What is the temperature in Celsius? '))
+fahrenheit_temp = celsius_to_fahrenheit(celsius_temp)
+print(f'In old fashioned Fahrenheit degrees, that is {fahrenheit_temp}')
