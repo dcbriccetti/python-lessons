5919327b9df7f408b157bda676a89a50aff3fa8f
Dave Briccetti <daveb@davebsoft.com>
2020-10-20 17:18:57 -0700

    Update air quality program

--- Web/Screen-Scraping/air_quality.py
+++ Web/Screen-Scraping/air_quality.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-while True:
-    state = input('What state? ').lower().replace(' ', '-')
-    city  = input('What city? ') .lower().replace(' ', '-')
-    response = requests.get(f'https://www.iqair.com/us/usa/{state}/{city}')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    aqi_elem = soup.find('p', class_='aqi-value__value')
-    if aqi_elem:
-        aqi_value = int(aqi_elem.text)
-        print(f'Air quality index: {aqi_value}')
-    else:
-        print('Not found')
+state = input('What state? (California) ').lower().replace(' ', '-') or "california"
+city  = input('What city? ') .lower().replace(' ', '-')
+response = requests.get(f'https://www.iqair.com/us/usa/{state}/{city}')
+soup = BeautifulSoup(response.text, 'html.parser')
+aqi_elem = soup.find('p', class_='aqi-value__value')
+if aqi_elem:
+    aqi_text = aqi_elem.text
+    aqi_value = int(aqi_text)
+    print(f'Air quality index: {aqi_value}')
+else:
+    print('Not found')
