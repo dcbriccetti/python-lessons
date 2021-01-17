15ffe726e39581db1c9c41fde5b94745501df0f7
Dave Briccetti <daveb@davebsoft.com>
2021-01-17 13:11:03 -0800

    Modernize weather fetcher

--- Web/weather.py
+++ Web/weather.py
@@ -1,12 +1,13 @@
 import requests
 
-key = open('openweathermap.txt').read().strip()  # Put your API key in this file
+with open('openweathermap.txt') as file:
+    key = file.read().strip()  # Put your API key in this file
 
 
 def temp_at_location(location):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&mode=json&units=metric&APPID=%s' % (location, key)
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&mode=json&units=metric&APPID={key}'
     json = requests.get(url).json()
     return json['main']['temp']
 
 if __name__ == '__main__':
-    print('%.1f' % temp_at_location('94549,US'))
+    print(f'{temp_at_location("94549"):.1f}')
