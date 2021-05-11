2a159ad5e1186c749b96c5d0ede45b7142c6bbb5
Dave Briccetti <daveb@davebsoft.com>
2021-05-11 13:40:04 -0700

    Show some statistics at the end

--- simple/mathquiz.py
+++ simple/mathquiz.py
@@ -2,6 +2,9 @@ from random import randint
 from time import time
 
 print('Enjoy a math quiz, and respond with nothing to stop.')
+num_right = 0
+num_wrong = 0
+sum_answer_time = 0
 
 while True:
     num1 = randint(2, 9)
@@ -15,6 +18,14 @@ while True:
     try:
         answer = int(response)
         elapsed = time() - start_time
-        print(f'Right, in {elapsed:.2f} seconds' if answer == product else f'Wrong, the answer was {product}')
+        if answer == product:
+            print(f'Right, in {elapsed:.2f} seconds')
+            num_right += 1
+            sum_answer_time += elapsed
+        else:
+            print(f'Wrong, the answer was {product}')
+            num_wrong += 1
     except ValueError:
         print('You entered something other than an integer')
+
+print(f'You got {num_right} right and {num_wrong} wrong. Average time for right answers: {sum_answer_time / num_right:.2f}')
