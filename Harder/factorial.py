4d9b25bf2b4dd46771b6d218b24a8cd09fbe7325
Dave Briccetti <daveb@davebsoft.com>
2022-01-17 14:59:15 -0800

    Simplify factorial program

--- Harder/factorial.py
+++ Harder/factorial.py
@@ -1,8 +1,5 @@
 def factorial(n):
-    if n == 1:
-        return n
-    else:
-        return n * factorial(n - 1)
+    return n if n == 1 else n * factorial(n - 1)
 
 assert(factorial(3) == 6)
 assert(factorial(4) == 24)
