adea4e6ad8d971b0ec0f6ccf01c55a7c81a1e019
Dave Briccetti <daveb@davebsoft.com>
2022-01-18 14:13:26 -0800

    Improve memory game code

--- Harder/memorygame.py
+++ Harder/memorygame.py
@@ -1,43 +1,38 @@
 from random import randint, choice, shuffle
 from time import sleep
+from typing import Sequence, Optional, Iterator
+
+def clear():
+    for n in range(100):  # “Clear” the console
+        print()
+
+def create_sequence(
+        include: Optional[int] = None,
+        exclude: Optional[Iterator[int]] = None) -> Sequence[int]:
+    unique_numbers: set[int] = {include} if include else set()
+    excluding: set[int] = exclude or set()
+    while len(unique_numbers) < seq_length:
+        candidate = randint(1, highest_num)
+        if candidate not in excluding:
+            unique_numbers.add(candidate)
+    unique_numbers_list = list(unique_numbers)
+    shuffle(unique_numbers_list)
+    return unique_numbers_list
 
 seq_length = 3
 highest_num = 20
 
-
-def clear():
-    for n in range(100):  # Clear the console
-        print()
-
-
 while True:
-    first_sequence = set()
-    while len(first_sequence) < seq_length:
-        first_sequence.add(randint(1, highest_num))
-    first_sequence = list(first_sequence)
-    shuffle(first_sequence)
-
+    first_sequence = create_sequence()
     clear()
-    print('Remember these numbers:')
-    print(first_sequence)
+    print(f'Remember these numbers:\n{first_sequence}')
     sleep(2)
 
     number_in_both_sequences = choice(first_sequence)
-
-    second_sequence = set()
-    second_sequence.add(number_in_both_sequences)
-    while len(second_sequence) < seq_length:
-        candidate = randint(1, highest_num)
-        if candidate not in first_sequence:
-            second_sequence.add(candidate)
-    second_sequence = list(second_sequence)
-    shuffle(second_sequence)
+    second_sequence = create_sequence(include=number_in_both_sequences, exclude=first_sequence)
 
     clear()
-    print('Which one of these numbers was in the previous sequence? ')
-    print(second_sequence)
-
-    response = input()
+    response = input(f'Which one of these numbers was in the previous sequence?\n{second_sequence}')
     if not response:
         break
     if response == str(number_in_both_sequences):
@@ -49,3 +44,4 @@ while True:
             seq_length -= 1
 
     sleep(1)
+
