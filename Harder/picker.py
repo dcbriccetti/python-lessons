b149142c8156027d9821e4e77dfdc79e8617a868
Dave Briccetti <daveb@davebsoft.com>
2022-01-19 10:28:06 -0800

    Modernize picker

--- Harder/picker.py
+++ Harder/picker.py
@@ -1,12 +1,12 @@
-# Pops names off a list in random sequence
+# This program pops names off a list in random sequence
 
 from random import randint
-import time
+from time import sleep
 
-remaining_names = ['sue', 'mary', 'hari', 'fred', 'eric', 'menlo']
+remaining_names = 'sue mary hari fred eric menlo'.split()
 
-while len(remaining_names) > 0:
+while remaining_names:
     pop_index = randint(0, len(remaining_names) - 1)
     popped_name = remaining_names.pop(pop_index)
     print(popped_name)
-    time.sleep(1)
+    sleep(1)
