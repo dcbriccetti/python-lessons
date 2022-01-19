93d0bb6768966700634043352e6378cc3477f564
Dave Briccetti <daveb@davebsoft.com>
2022-01-19 09:33:36 -0800

    Improve Mr. T. program

--- Harder/MrTGuessTheFood.py
+++ Harder/MrTGuessTheFood.py
@@ -1,22 +1,22 @@
 from random import choice
 
-items = ('soup', 'corn', 'milk', 'bread', 'eggs')
+items = ['soup', 'corn', 'milk', 'bread', 'eggs']
 item = choice(items)
-positiveSayings = ('Stay in school!', "Don't do drugs!")
+positiveSayings = ['Stay in school!', "Don't do drugs!"]
 
-guess = ''
+guess = None
 guessesLeft = 3
-print('Mr. T says guess the food item in %d guesses.' % guessesLeft)
-print('Items: ' + ', '.join(items))
+print(f'Mr. T says guess the food item in {guessesLeft} guesses.')
+print('Items:', ', '.join(items))
 
-while guess != item and guessesLeft > 0:
+while guess != item and guessesLeft:
     guess = input('Your guess? ')
     guessesLeft -= 1
-    if guess == item:
-        print("That's right. " + choice(positiveSayings))
-    elif guessesLeft > 0:
-        singularOrPlural = 'guess' if guessesLeft == 1 else 'guesses'
-        print('Wrong. %d %s left.' % (guessesLeft, singularOrPlural))
+    if guess != item and guessesLeft:
+        singularOrPlural = 'guess' + ('' if guessesLeft == 1 else 'es')
+        print(f'Wrong. {guessesLeft} {singularOrPlural} left.')
 
-if guess != item:
+if guess == item:
+    print("That's right.", choice(positiveSayings))
+else:
     print("I pity the fool!")
