5a4359473c26e3252078e8e63b904ed2ba979508
Dave Briccetti <daveb@davebsoft.com>
2022-01-20 14:04:28 -0800

    Change runways program to use different data and to use Pandas

--- Harder/runways.py
+++ Harder/runways.py
@@ -1,36 +1,53 @@
-'''A starting point for doing some things with runway information'''
+'Give information about airports and their runways'
 
-from random import choice
-import csv
+from os import mkdir, path
 
+import pandas as pd
+import requests
 
-class Runway:
-    def __init__(self, airport_id, runway_name):
-        self.airport_id = airport_id
-        self.runway_name = runway_name
+def run():
+    DATA_DIR = 'https://github.com/davidmegginson/ourairports-data/raw/main'
+    CACHE_DIR = 'aviation-cache'
 
-    def __str__(self):
-        return self.airport_id + ' ' + self.runway_name
+    def cache_files_if_needed():
+        try:
+            mkdir(CACHE_DIR)
+        except FileExistsError:
+            pass
+        for filename in ['airports.csv', 'runways.csv']:
+            path_fn = f'{CACHE_DIR}/{filename}'
+            if not path.exists(path_fn):
+                print('fetching', path_fn)
+                with open(path_fn, 'w') as f:
+                    f.write(requests.get(f'{DATA_DIR}/{filename}').text)
 
-airports_by_id = {}
+    def create_airports_df():
+        cols = 'name ident type latitude_deg longitude_deg elevation_ft iso_country iso_region municipality'.split()
+        df = pd.read_csv(f'{CACHE_DIR}/airports.csv', index_col='ident', usecols=cols)
+        df['type'] = df['type'].astype('category')
+        return df
 
-# Data come from from http://ourairports.com/data/
-with open("/Users/daveb/Documents/Flying/data/airports.csv") as file:
-    reader = csv.reader(file)
-    for line in reader:
-        airport_id = line[1]
-        airport_name = line[3]
-        airports_by_id[airport_id] = airport_name
+    def create_runways_df():
+        cols = 'id airport_ref airport_ident length_ft width_ft surface closed le_ident he_ident'.split()
+        return pd.read_csv(f'{CACHE_DIR}/runways.csv', index_col='id', usecols=cols)
 
-runways = []
+    cache_files_if_needed()
+    airports = create_airports_df()
+    runways = create_runways_df()
 
-with open("/Users/daveb/Documents/Flying/data/runways.csv") as file:
-    reader = csv.reader(file)
-    for line in reader:
-        airport_id = line[2]
-        runway_name = line[8]
-        runways.append(Runway(airport_id, runway_name))
+    while True:
+        if not (ident := input('\nWhat airport ID? ').strip().upper()):
+            break
+        if ident in airports.index:
+            ap = airports.loc[ident]
+            print(f"{ap['name']} is in {ap['iso_region']}, {ap['municipality']}, "
+                  f"elev. {round(ap['elevation_ft'])} ft")
+            print('Runways')
+            airport_rws = runways[runways['airport_ident'] == ident]
+            for row in airport_rws.itertuples():
+                print(f"{row.le_ident}/{row.he_ident}: {round(row.length_ft)}×{round(row.width_ft)} ft")
+        else:
+            print('Can’t find that airport')
 
-runway = choice(runways)
-airport = airports_by_id[runway.airport_id]
-print(airport, runway)
+if __name__ == '__main__':
+    run()
