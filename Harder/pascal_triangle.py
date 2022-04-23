ff5fd2d6218d03c481c68446b38d5a8c93680f12
Dave Briccetti <daveb@davebsoft.com>
2022-04-22 19:06:49 -0700

    Create new Pascal’s Triangle program

---
+++ Harder/pascal_triangle.py
@@ -0,0 +1,19 @@
+'Pascal’s Triangle Maker'
+
+from itertools import pairwise
+
+def make_triangle(num_rows=10):
+    def make_next_row(row: list[int]) -> list[int]:
+        return [1] + [left + right for left, right in pairwise(row)] + [1]
+
+    def format_row(row_numbers: list[int]):
+        centered_padded_nums: list[str] = [f'{num: ^3}' for num in row_numbers]
+        return f"{' '.join(centered_padded_nums): ^40}"
+
+    row = [1]
+    for _ in range(num_rows):
+        print(format_row(row))
+        row = make_next_row(row)
+
+if __name__ == '__main__':
+    make_triangle()
