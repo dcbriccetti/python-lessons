14737ac0d52c3bda6e24a6b36b31bac0633cc740
Dave Briccetti <daveb@davebsoft.com>
2023-04-06 15:14:53 -0700

    Improve code

--- simple/mathquiz.py
+++ simple/mathquiz.py
@@ -4,28 +4,28 @@ from time import time
 print('Enjoy a math quiz, and respond with nothing to stop.')
 num_right = 0
 num_wrong = 0
-sum_answer_time = 0
+total_right_answer_seconds = 0.0
 
 while True:
     num1 = randint(2, 9)
     num2 = randint(11, 99)
     product = num1 * num2
     start_time = time()
-    response = input(f'What is {num1} * {num2}? ')
+    response = input(f'What is {num1} × {num2}? ')
     if not response:
         break
-
-    try:
-        answer = int(response)
-        elapsed = time() - start_time
-        if answer == product:
+    elapsed = time() - start_time
+    try:  # `int` may fail
+        if int(response) == product:
             print(f'Right, in {elapsed:.2f} seconds')
             num_right += 1
-            sum_answer_time += elapsed
+            total_right_answer_seconds += elapsed
         else:
             print(f'Wrong, the answer was {product}')
             num_wrong += 1
     except ValueError:
         print('You entered something other than an integer')
 
-print(f'You got {num_right} right and {num_wrong} wrong. Average time for right answers: {sum_answer_time / num_right:.2f}')
+print(f'You got {num_right} right and {num_wrong} wrong.')
+if num_right:
+    print(f'Average time for right answers: {total_right_answer_seconds / num_right :.2f} seconds.')
