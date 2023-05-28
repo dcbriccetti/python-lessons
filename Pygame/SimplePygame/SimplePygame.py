23fe039846e46338b175d0e409c61cc20a97738c
Dave Briccetti <daveb@davebsoft.com>
2023-05-28 11:59:43 -0700

    Improve readability

--- Pygame/SimplePygame/SimplePygame.py
+++ Pygame/SimplePygame/SimplePygame.py
@@ -8,19 +8,18 @@ bee = pygame.image.load('bee1.png').convert_alpha()
 beeX = 0
 beeY = 0
 clock = pygame.time.Clock()
+running = True
 
-loop = True
-while loop:
+while running:
     for event in pygame.event.get():
-        if event.type == QUIT \
-            or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            loop = False
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            running = False
 
-    keystate = pygame.key.get_pressed()
-    if keystate[K_RIGHT]:
+    keys = pygame.key.get_pressed()
+    if keys[K_RIGHT]:
         beeX += 5
 
-    screen.fill((0,120,0))
+    screen.fill((0, 120, 0))
     screen.blit(bee, (beeX, beeY))
     pygame.display.flip()
     clock.tick(60)
